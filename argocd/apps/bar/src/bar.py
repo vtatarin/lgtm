da0225ad30858e2b6ea624a9feb3c2b7c2cd1de3
Valerii Tatarin <valerii.tatarin@gmail.com>
2024-05-18 23:02:26 -0700

    foo, bar: init

---
+++ argocd/apps/bar/src/bar.py
@@ -0,0 +1,75 @@
+from time import sleep
+from flask import Flask, request
+from flask.logging import default_handler
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import logging
+import os
+from random import random
+
+AGENT_HOSTNAME = os.getenv("AGENT_HOSTNAME", "localhost")
+AGENT_PORT = int(os.getenv("AGENT_PORT", "4317"))
+
+
+class SpanFormatter(logging.Formatter):
+    def format(self, record):
+        trace_id = trace.get_current_span().get_span_context().trace_id
+        if trace_id == 0:
+            record.trace_id = None
+        else:
+            record.trace_id = "{trace:032x}".format(trace=trace_id)
+        return super().format(record)
+
+
+resource = Resource(attributes={
+    "service.name": os.getenv("SERVICE", "bar")
+})
+
+trace.set_tracer_provider(
+    TracerProvider(resource=resource)
+)
+otlp_exporter = OTLPSpanExporter(endpoint=f"{AGENT_HOSTNAME}:{AGENT_PORT}", insecure=True)
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+FlaskInstrumentor().instrument_app(app)
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+app.logger.setLevel(logging.INFO)
+default_handler.setFormatter(
+    SpanFormatter(
+        'time="%(asctime)s" service=%(name)s level=%(levelname)s %(message)s trace_id=%(trace_id)s'
+    )
+)
+
+
+@app.route("/bar")
+def bar():
+    random_value = random()  # random number in range [0.0,1.0)
+    if random_value < 0.2:
+        return "Edge case!", 500
+    return "bar"
+
+
+@app.after_request
+def after_request(response):
+    app.logger.info(
+        'addr="%s" method=%s scheme=%s path="%s" status=%s',
+        request.remote_addr,
+        request.method,
+        request.scheme,
+        request.full_path,
+        response.status_code,
+    )
+    return response
---
+++ argocd/apps/foo/src/foo.py
@@ -0,0 +1,84 @@
+from flask import Flask, request
+from flask.logging import default_handler
+import requests
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import cowsay
+import logging
+import os
+
+AGENT_HOSTNAME = os.getenv("AGENT_HOSTNAME", "localhost")
+AGENT_PORT = int(os.getenv("AGENT_PORT", "4317"))
+BAR_ENDPOINT = os.getenv("BAR_ENDPOINT", "http://localhost:5001/bar")
+
+
+class SpanFormatter(logging.Formatter):
+    def format(self, record):
+        trace_id = trace.get_current_span().get_span_context().trace_id
+        if trace_id == 0:
+            record.trace_id = None
+        else:
+            record.trace_id = "{trace:032x}".format(trace=trace_id)
+        return super().format(record)
+
+resource = Resource(attributes={
+    "service.name": os.getenv("SERVICE", "foo")
+})
+
+trace.set_tracer_provider(
+    TracerProvider(resource=resource)
+)
+otlp_exporter = OTLPSpanExporter(endpoint=f"{AGENT_HOSTNAME}:{AGENT_PORT}", insecure=True)
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+app.logger.setLevel(logging.INFO)
+default_handler.setFormatter(
+    SpanFormatter(
+        'time="%(asctime)s" service=%(name)s level=%(levelname)s %(message)s trace_id=%(trace_id)s'
+    )
+)
+
+
+@app.route("/foo")
+def foo():
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("bar-request"):
+        r = requests.get(BAR_ENDPOINT)
+        if r.status_code != 200:
+            return "Error!", r.status_code
+
+    return "foo" + r.text
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return cowsay.get_output_string('cow', path)
+
+@app.after_request
+def after_request(response):
+    app.logger.info(
+        'addr="%s" method=%s scheme=%s path="%s" status=%s',
+        request.remote_addr,
+        request.method,
+        request.scheme,
+        request.full_path,
+        response.status_code,
+    )
+    return response
