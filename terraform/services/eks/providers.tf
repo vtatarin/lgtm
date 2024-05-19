edd846d2035dbb2e66eeeb67f0dd1cfcdb5382d5
Valerii Tatarin <valerii.tatarin@gmail.com>
2024-05-19 11:57:11 -0700

    terraform: update env config

--- terraform/services/eks/providers.tf
+++ terraform/services/eks/providers.tf
@@ -8,7 +8,6 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "lgtm-playground-tfstate-20240510003852147300000001"
-    key    = "437023642520/us-east-2/sandbox/eks"
     region = "us-east-2"
   }
   required_version = "~> 1.8"
